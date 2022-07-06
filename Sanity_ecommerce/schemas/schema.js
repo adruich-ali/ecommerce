bd8d60f1f39cf9a7be39deb6fa350cb02ce2d924
adruich ali <adruich.ali@gmail.com>
2022-07-06 15:26:47 +0100

    oka

---
+++ Sanity_ecommerce/schemas/schema.js
@@ -0,0 +1,17 @@
+// First, we must import the schema creator
+import createSchema from 'part:@sanity/base/schema-creator'
+// Then import schema types from any plugins that might expose them
+import schemaTypes from 'all:part:@sanity/base/schema-type'
+
+// We import object and document schemas
+import product from './product'
+import banner from './banner'
+
+
+export default createSchema({
+  name: 'default',
+  types: schemaTypes.concat([
+    product,
+    banner
+  ]),
+})
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
+
+import React from 'react';
+import { Toaster } from 'react-hot-toast';
+
+import { Layout } from '../commponents';
+import '../styles/globals.css';
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+   
+      <Layout>
+        
+        <Component {...pageProps} />
+      </Layout>
+
+  )
 }
 
 export default MyApp
